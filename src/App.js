96515e113e4d4a3cb80f005f9087a2fdd7b08f07
SeunghoLee <jimmylee871124@gmail.com>
2019-03-09 21:43:16 +0900

    [FEAT]Add Movie, MoviePoster Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
+
+const movies = [
+  {
+    title: "Matrix",
+    poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+  },
+  {
+    title: "Full metal Jacket",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/41MN0ANVJTL._SY445_.jpg"
+  },
+  {
+    title: "Oldboy",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXepaTQOs33GBlFS1Lu0Y6ifwsINileFw1TxhC62NId7VsRmxg"
+  },
+  {
+    title: "Star Wars",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoBq5af1KcwELZNebyBPZR0Vbywc71omi5oe7NqsoPv3J2cDFGAw"
+  }
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {movies.map((movie, index) => {
+          return <Movie title={movie.title} poster={movie.poster} key={index} />
+        })}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* es6에선 _가 default exports를 의미하며, 
+each나 forEach는 named exports를 의미한다.
+따라서 다음과 같이 default exports와 named exports를
+동시에 얻는 방법도 가능하다.
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+//test.js
+export default function(obj) {
+
+}
+export function each(obj, iterator, context){
+
+}
+export { each as forEach };
+
+import _, { each } from 'test';
+*/
+
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.register();
 serviceWorker.unregister();
